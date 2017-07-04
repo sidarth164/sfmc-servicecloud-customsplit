d82ea7e70f1c22ccba522200af88e88a1e305f65
Siddharth Agrawal <sidarth@iitk.ac.in>
2017-07-04 12:15:08 +0000

    Update sfdc.js

--- backend/lib/sfdc.js
+++ backend/lib/sfdc.js
@@ -77,7 +77,7 @@ class ServiceCloud {
 		if (!/^([a-zA-Z0-9]{5})$/g.test(id)) return cb(new Error('Invalid Salesforce-Id given.'));
 
 		const self = this;
-		const query = "SELECT Axtria_ID, isRegistered FROM Contact WHERE Axtria_ID = id";
+		const query = "SELECT Axtria_ID, isRegistered FROM Contact WHERE Axtria_ID = \'' + id + '\'";
 
 		// Login if necessary
 		self._login((e) => {
