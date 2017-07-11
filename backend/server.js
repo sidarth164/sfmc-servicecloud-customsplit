defcafcd3a3951c99d10386c4d8680e24a164fb3
Siddharth Agrawal <sidarth@iitk.ac.in>
2017-07-11 12:54:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,15 @@ app.post('/activity/execute', (req, res) => {
 
 		if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
 			var serviceCloudId = 'A1375';
-			console.log(decoded.inArguments.serviceCloudId);
+			var aArgs = decoded.inArguments;
+			var oArgs = {};
+			for (var i=0; i<aArgs.length; i++) {  
+				for (var key in aArgs[i]) { 
+					oArgs[key] = aArgs[i][key]; 
+				}
+			}
+
+			console.log(oArgs.serviceCloudId);
 			// TODO: Read the Service Cloud object's Id from inArguments here and
 			// write it to the serviceCloudId variable
 
